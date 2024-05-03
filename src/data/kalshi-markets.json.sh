e8126a279c3a980ab25deb229d83e3b0d79d9f4f
0xperp <0xperp@protonmail.com>
2024-05-03 09:14:53 -0400

    pull current day or prior if metrics are not posted yet

--- src/data/kalshi-markets.json.sh
+++ src/data/kalshi-markets.json.sh
@@ -1,1 +1,13 @@
-curl "https://kalshi-public-docs.s3.amazonaws.com/reporting/market_data_$(date -d yesterday +'%Y-%m-%d').json"   -H 'Connection: keep-alive'   -A 'Chrome/123.0.0.0'   --compressed
+response=$(curl -s -o /dev/null -w "%{http_code}" "https://kalshi-public-docs.s3.amazonaws.com/reporting/market_data_$(date +'%Y-%m-%d').json")
+
+if [ $response -eq 200 ]; then
+    curl "https://kalshi-public-docs.s3.amazonaws.com/reporting/market_data_$(date +'%Y-%m-%d').json" \
+      -H 'Connection: keep-alive' \
+      -A 'Chrome/123.0.0.0' \
+      --compressed
+else
+    curl "https://kalshi-public-docs.s3.amazonaws.com/reporting/market_data_$(date -d yesterday +'%Y-%m-%d').json" \
+      -H 'Connection: keep-alive' \
+      -A 'Chrome/123.0.0.0' \
+      --compressed
+fi
