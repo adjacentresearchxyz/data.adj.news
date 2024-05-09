49a13f0c7226e814edfeac2494608ebfa0669593
0xperp <0xperp@protonmail.com>
2024-05-08 20:04:06 -0400

    est

--- src/data/kalshi/kalshi-markets.json.sh
+++ src/data/kalshi/kalshi-markets.json.sh
@@ -1,12 +1,12 @@
-response=$(curl -s -o /dev/null -w "%{http_code}" "https://kalshi-public-docs.s3.amazonaws.com/reporting/market_data_$(date +'%Y-%m-%d').json")
+response=$(curl -s -o /dev/null -w "%{http_code}" "https://kalshi-public-docs.s3.amazonaws.com/reporting/market_data_$(TZ=EST date +'%Y-%m-%d').json")
 
 if [ $response -eq 200 ]; then
-    curl "https://kalshi-public-docs.s3.amazonaws.com/reporting/market_data_$(date +'%Y-%m-%d').json" \
+    curl "https://kalshi-public-docs.s3.amazonaws.com/reporting/market_data_$(TZ=EST date +'%Y-%m-%d').json" \
       -H 'Connection: keep-alive' \
       -A 'Chrome/123.0.0.0' \
       --compressed
 else
-    curl "https://kalshi-public-docs.s3.amazonaws.com/reporting/market_data_$(date -d yesterday +'%Y-%m-%d').json" \
+    curl "https://kalshi-public-docs.s3.amazonaws.com/reporting/market_data_$(TZ=EST date -d yesterday +'%Y-%m-%d').json" \
       -H 'Connection: keep-alive' \
       -A 'Chrome/123.0.0.0' \
       --compressed
