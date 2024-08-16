a448817acd4f0f7cdad2d99c96444f0c98387f5b
0xperp <0xperp@protonmail.com>
2024-08-16 18:23:27 -0400

    postgres

--- src/data/api/postgres.ts
+++ src/data/api/postgres.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import type {Row, Sql} from "postgres";
 import postgres from "postgres";
 
-const POSTGRES_URL = process.env.NEXT_PUBLIC_POSTGRES_URL;
+const POSTGRES_URL = process.env.POSTGRES_URL;
 
 if (!POSTGRES_URL) throw new Error("Missing POSTGRES_URL");
 
