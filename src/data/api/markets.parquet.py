556426ff308f727b9e2b43025603be91696e3f8e
0xperp <0xperp@protonmail.com>
2024-10-24 12:16:20 -0400

    update

--- src/data/api/markets.parquet.py
+++ src/data/api/markets.parquet.py
@@ -3,4 +3,4 @@ import os
 
 token = os.environ.get('MOTHERDUCK_TOKEN')
 con = duckdb.connect(f'md:adjacent?{token}')
-con.sql("COPY (SELECT ticker, adj_ticker, end_date, question, description, status, rules, category, probability, platform FROM markets_data WHERE current_date <= end_date ORDER BY end_date DESC) TO STDOUT (FORMAT 'parquet', COMPRESSION 'gzip');")
+con.sql("COPY (SELECT ticker, adj_ticker, end_date, question, description, status, rules, category, probability, platform FROM markets_data WHERE (markets_data.status = 'active' OR markets_data.status = 'true') AND current_date <= end_date ORDER BY end_date DESC) TO STDOUT (FORMAT 'parquet', COMPRESSION 'gzip');")
